1533b3fa7fb412f4cd100151e6b3764570c7801a
Xeus <nsaghakingsly@gmail.com>
2022-10-24 18:19:54 +0100

    view updates for unauthenticated user

--- resources/js/Layouts/VisitorLayout.vue
+++ resources/js/Layouts/VisitorLayout.vue
@@ -11,9 +11,7 @@ import { Link } from '@inertiajs/inertia-vue3';
         </div>
     </div>
 
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div class="">
-            <slot />
-        </div>
+    <div class="">
+        <slot />
     </div>
 </template>
